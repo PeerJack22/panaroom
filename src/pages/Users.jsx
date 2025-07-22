6f6318edde4f24c8390fcae0ebd290f6c3afa3d3
PeerJack22 <pajocina.11@gmail.com>
2025-07-22 12:03:48 -0500

    Estructura para mas funcionalidades

---
+++ src/pages/Users.jsx
@@ -0,0 +1,11 @@
+const Users = () => {
+    return (
+        <div>
+            <h1 className='font-black text-4xl text-gray-500'>Usuarios</h1>
+            <hr className='my-4 border-t-2 border-gray-300' />
+            <p className='mb-8'>Este módulo te permite gestionar los usuarios</p>
+        </div>
+    )
+}
+
+export default Users
