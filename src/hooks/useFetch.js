8af8306d0994a9dc38f320e0b3c8cd3282b7e196
PeerJack22 <pajocina.11@gmail.com>
2025-08-05 10:34:05 -0500

    Subir imagenes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,30 +2,33 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 function useFetch() {
-    const fetchDataBackend = async (url, data = null, method = "GET",headers = {}) => {
+    const fetchDataBackend = async (url, data = null, method = "GET", headers = {}) => {
         const loadingToast = toast.loading("Procesando solicitud...");
         try {
-            const options = {
-            method,
-            url,
-            headers: {
-                "Content-Type": "application/json",
-                ...headers,
-            },
-            data,
-            }
-            const response = await axios(options)
-            toast.dismiss(loadingToast); 
-            toast.success(response?.data?.msg)
-            return response?.data
-        } catch (error) {
-            toast.dismiss(loadingToast); 
-            console.error(error)
-            toast.error(error.response?.data?.msg)
-        }
-    }
+            // Detecta si data es FormData
+            const isFormData = data instanceof FormData;
 
-    return { fetchDataBackend }
+            const options = {
+                method,
+                url,
+                headers: isFormData
+                    ? { ...headers } // NO agregues Content-Type si es FormData
+                    : { "Content-Type": "application/json", ...headers },
+                data,
+            };
+
+            const response = await axios(options);
+            toast.dismiss(loadingToast);
+            toast.success(response?.data?.msg);
+            return response?.data;
+        } catch (error) {
+            toast.dismiss(loadingToast);
+            console.error(error);
+            toast.error(error.response?.data?.msg);
+        }
+    };
+
+    return { fetchDataBackend };
 }
 
 export default useFetch;
